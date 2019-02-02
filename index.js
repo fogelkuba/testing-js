9ec13e2d200c1900561df6c53262a346ca6e00aa
fogelkuba <fogelkuba@gmail.com>
2019-02-02 12:43:28 +0100

    third

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-function add (number1, number2) {
-    // return number1 + number2; //should work correctly
-    return number1 - number2;    // will not work
+function add (num1, num2) {
+
+    if (!num1 && !num2) {
+        throw new Error('Numbers are required for testing')
+    }
+
+    return num1 + num2; //should work correctly
+    // return num1 - num2;    // will not work
 }
 
 
@@ -8,4 +13,4 @@ module.exports = {
     add
 }
 
-console.log(add(2,3.5))
+// console.log(add(2,3.5))
--- index.spec.js
+++ index.spec.js
@@ -1,7 +1,30 @@
+const assert = require('assert');
 const { add } = require('./index');
 
-const result = add(5, 6)
 
-if (result !== 11) {
-    throw new Error('The add function did not return correct result when adding 5 and 6')
-}
+console.log('First Test');
+
+    const actual = add(5, 6)
+    const expected = 11;
+
+    assert.equal(actual, expected)
+
+    // this is assertion
+    // if (actual !== expected) {
+    //     throw new Error(`The add function did not return expected value (${expected}) when adding 5 and 6`)
+    // }
+
+    console.log('--> Test passed')
+
+console.log('End of First Test');
+
+
+console.log('Second Test: Check if error occurs when no arguments are passed');
+
+    assert.throws(() => {
+        add()
+    })
+
+    console.log('--> Test passed')
+
+console.log('End of Second Test');
