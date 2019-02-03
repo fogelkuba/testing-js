e5cec305938934b47b75cb0d720a91f4a2ef8687
fogelkuba <fogelkuba@gmail.com>
2019-02-03 14:05:32 +0100

    async test

--- src/test/async.spec.js
+++ src/test/async.spec.js
@@ -8,14 +8,30 @@ describe('The Async function test', function () {
                 assert.equal(result.user.name, 'bahdcoder')
                 done();
             });
-            
         })
+        it('Should return "Miss Myers" found by id: 2', async () => {
+            const result = await findUserById(2);
+            assert.equal(result.user.name, 'Miss Myers')
+        })
+        it('Should throw err when user was not found by id', () => {
+            const id = 123123;
+            return findUserById(id).catch(error => {
+                assert.equal(error.message, `User with id: ${id} was not found.`)
+            })
+        });
     })
     describe('The findUserByEmial function test', () => {
         it('Should return user by email', function () {
-            return findUserById(3).then(result => {
+            return findUserByEmail('emily@gates.co.za').then(result => {
+                assert.equal(result.user.id, 3)
                 assert.equal(result.user.email, 'emily@gates.co.za')
             });
-        })
+        });
+        it('Should throw err when user was not found by email', () => {
+            const email = 'johny@test.co.za';
+            return findUserByEmail(email).catch(error => {
+                assert.equal(error.message, `User with email: ${email} was not found.`)
+            })
+        });
     })
 })
